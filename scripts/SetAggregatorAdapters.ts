4efdfc1a3de93244c4c4820120402be80b589a73
Kingsley Victor <javaprodigy56@gmail.com>
2024-08-18 15:52:44 +0100

    made updates

---
+++ scripts/SetAggregatorAdapters.ts
@@ -0,0 +1,42 @@
+import { network } from "hardhat";
+import { join } from "path";
+import deployedContracts from "./constants/output/AggregatorOutput.json";
+import { getContractAt } from "./utils/helpers";
+import { AggregatorRouter } from "../artifacts/types";
+import { writeFile, readFile } from "fs/promises";
+
+interface AggregatorOutput {
+  Adapters: string[];
+  Router: string;
+}
+
+async function main() {
+  const chainId = network.config.chainId as number;
+  const deployedC = deployedContracts[chainId as unknown as keyof typeof deployedContracts];
+  const Adapters = deployedC.Adapters;
+  Adapters.pop();
+
+  const aggregatorRouter = await getContractAt<AggregatorRouter>("AggregatorRouter", deployedC.Router);
+  await aggregatorRouter.setAdapters(Adapters);
+
+  const output: AggregatorOutput = {
+    ...deployedC,
+    Adapters
+  };
+
+  const outputDirectory = "scripts/constants/output";
+  const outputFile = join(process.cwd(), outputDirectory, "AggregatorOutput.json");
+
+  try {
+    const buf = await readFile(outputFile);
+    const contents = JSON.parse(buf.toString());
+    await writeFile(outputFile, JSON.stringify({ ...contents, [network.config.chainId as any]: output }, null, 2));
+  } catch (err) {
+    console.error(`Error writing output file: ${err}`);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
